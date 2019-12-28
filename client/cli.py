94ed0ac0a3ddf36cb4be54f5e9705e52114d72b6
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2019-12-28 13:14:24 +0530

    signup and login added

--- client/cli.py
+++ client/cli.py
@@ -1,2 +1,28 @@
 import click
+import keyring
+import requests
 from PyInquirer import style_from_dict,Token,Separator,prompt
+
+@click.group()
+def main():
+    """Simple CLI tool for storing contacts in System"""
+    pass
+
+@main.command()
+@click.option('--name', prompt='Your Name', required=True, type=str)
+@click.option('--email', prompt='Your Email', required=True, type=str)
+@click.option('--password', prompt='Your Password', required=True, type=str,hide_input=True, confirmation_prompt=True)
+def signup(name, email, password):
+    click.echo(password)
+
+
+@main.command()
+@click.option('--name', prompt='Your Email', required=True, type=str)
+@click.option('--password', prompt='Your Password',required=True, type=str, hide_input=True, confirmation_prompt=True)
+def login(name, password):
+    click.echo(name)
+    click.echo(password)
+
+
+if __name__ == "__main__":
+    main()
